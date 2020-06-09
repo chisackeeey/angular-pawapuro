cacefcbc0e9c74729b8eacf5bbf726fcb785659e
chisackeeey <chisaki_h@outlook.jp>
2020-06-09 13:46:58 +0900

    add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SelectTeamsComponent } from './select-teams/select-teams.component';
+import { SelectPitcherComponent } from './select-pitcher/select-pitcher.component';
+import { SelectBatterComponent } from './select-batter/select-batter.component';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectTeamsComponent,
+    SelectPitcherComponent,
+    SelectBatterComponent,
+    ConfirmationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/select-batter/select-batter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-select-batter',
+  templateUrl: './select-batter.component.html',
+  styleUrls: ['./select-batter.component.css']
+})
+export class SelectBatterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
