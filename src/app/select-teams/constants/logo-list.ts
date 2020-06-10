2ee4042aa4212721d2eb1eb1fcda807f783ac479
chisackeeey <chisaki_h@outlook.jp>
2020-06-10 15:25:00 +0900

    add constants

---
+++ src/app/select-teams/constants/logo-list.ts
@@ -0,0 +1,54 @@
+export const logoList = {
+  pacfic: [
+    {
+      name: 'lions',
+      path: '/assets/images/logo/lions.jpg',
+    },
+    {
+      name: 'fighters',
+      path: '/assets/images/logo/fighters.jpg',
+    },
+    {
+      name: 'lions',
+      path: '/assets/images/logo/lions.jpg',
+    },
+    {
+      name: 'fighters',
+      path: '/assets/images/logo/fighters.jpg',
+    },
+    {
+      name: 'lions',
+      path: '/assets/images/logo/lions.jpg',
+    },
+    {
+      name: 'fighters',
+      path: '/assets/images/logo/fighters.jpg',
+    },
+  ],
+  central: [
+    {
+      name: 'giants',
+      path: '/assets/images/logo/giants.jpg',
+    },
+    {
+      name: 'carp',
+      path: '/assets/images/logo/carp.jpg',
+    },
+    {
+      name: 'giants',
+      path: '/assets/images/logo/giants.jpg',
+    },
+    {
+      name: 'carp',
+      path: '/assets/images/logo/carp.jpg',
+    },
+    {
+      name: 'giants',
+      path: '/assets/images/logo/giants.jpg',
+    },
+    {
+      name: 'carp',
+      path: '/assets/images/logo/carp.jpg',
+    },
+  ],
+};
