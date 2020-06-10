04719212aad4d5a83ccc70d84131e71f94fe78ad
chisackeeey <chisaki_h@outlook.jp>
2020-06-10 15:25:26 +0900

    add select-teams component

--- src/app/select-teams/select-teams.module.ts
+++ src/app/select-teams/select-teams.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { SelectTeamsRoutingModule } from './select-teams-routing.module';
 import { SelectTeamsComponent } from './select-teams.component';
+import { TeamLogoComponent } from '../shared/view/team-logo/team-logo.component';
 
 @NgModule({
-  declarations: [SelectTeamsComponent],
+  declarations: [SelectTeamsComponent, TeamLogoComponent],
   imports: [CommonModule, SelectTeamsRoutingModule],
 })
 export class SelectTeamsModule {}
