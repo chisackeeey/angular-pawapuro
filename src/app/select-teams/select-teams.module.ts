00515f0d13477780313b537b6408ed37a64f1881
chisackeeey <chisaki_h@outlook.jp>
2020-06-10 16:07:08 +0900

    remove declarations

--- src/app/select-teams/select-teams.module.ts
+++ src/app/select-teams/select-teams.module.ts
@@ -3,10 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { SelectTeamsRoutingModule } from './select-teams-routing.module';
 import { SelectTeamsComponent } from './select-teams.component';
-import { TeamLogoComponent } from '../shared/view/team-logo/team-logo.component';
 
 @NgModule({
-  declarations: [SelectTeamsComponent, TeamLogoComponent],
+  declarations: [SelectTeamsComponent],
   imports: [CommonModule, SelectTeamsRoutingModule],
 })
 export class SelectTeamsModule {}
