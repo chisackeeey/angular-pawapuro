3584a86bf82183b1519d8df1c436dac02d3f2f0d
chisackeeey <chisaki_h@outlook.jp>
2020-06-12 16:30:38 +0900

    edit component

--- src/app/select-pitcher/select-pitcher.component.ts
+++ src/app/select-pitcher/select-pitcher.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { pitcherList } from './constants/pitcher-list';
+
 @Component({
   selector: 'app-select-pitcher',
   templateUrl: './select-pitcher.component.html',
-  styleUrls: ['./select-pitcher.component.css']
+  styleUrls: ['./select-pitcher.component.scss'],
 })
 export class SelectPitcherComponent implements OnInit {
+  starter = pitcherList.starter;
+  reserverList = pitcherList.reserver;
+  offList = pitcherList.off;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
